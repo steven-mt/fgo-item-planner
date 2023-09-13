33f64b2bbef39e4f67e19af08fa921bfd6dbe712
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-09-14 00:19:06 +0700

    Test auth

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,17 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+// import type { Database } from "@/lib/database.types";
+
+export async function GET(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
+
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(new URL("/account", req.url));
+}
