a992f2ce74037f0ac5ea9a9d91f89e3a9351f9ae
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-09-15 15:42:49 +0700

    Test environment variables for redirect URL

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -7,6 +7,23 @@ import { Database } from "./types/supabase";
 export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
 
+  const getURL = () => {
+    console.log(process?.env?.NEXT_PUBLIC_SITE_URL);
+    console.log(process?.env?.NEXT_PUBLIC_VERCEL_URL);
+
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+  };
+
   return (
     <Auth
       supabaseClient={supabase}
@@ -15,7 +32,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo={getURL()}
     />
   );
 }
