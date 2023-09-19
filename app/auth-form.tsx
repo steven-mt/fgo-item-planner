6f17dd3b8b49a60710f07736aa3871a7f4cb78bc
steven-mt <80749704+steven-mt@users.noreply.github.com>
2023-09-19 16:01:20 +0700

    Switch to server-side authentication

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -1,38 +1,17 @@
-"use client";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { Database } from "./types/supabase";
-
 export default function AuthForm() {
-  const supabase = createClientComponentClient<Database>();
-
-  const getURL = () => {
-    console.log(process?.env?.NEXT_PUBLIC_SITE_URL);
-    console.log(process?.env?.NEXT_PUBLIC_VERCEL_URL);
-
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      "http://localhost:3000/";
-
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`;
-
-    // Make sure to include a trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-    return url;
-  };
-
   return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
-      showLinks={false}
-      providers={[]}
-      redirectTo={getURL()}
-    />
+    <>
+      <div className="flex w-full flex-col items-center">
+        <form
+          className="w-60 border-black bg-gray-200 text-center"
+          action="/auth-server/login"
+          method="post"
+        >
+          <label htmlFor="email">Email</label>
+          <input className="w-full bg-gray-200" name="email" required />
+          <button>Sign In</button>
+        </form>
+      </div>
+    </>
   );
 }
