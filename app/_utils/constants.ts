1cab529c141944f5b910a344ad0f612916da35a5
steven-mt <80749704+steven-mt@users.noreply.github.com>
2024-01-13 23:08:34 +0700

    Update ascension constants

--- app/_utils/constants.ts
+++ app/_utils/constants.ts
@@ -1,5 +1,3 @@
-import { AscensionLevel } from "../_types/servant";
-
 export const ATLAS_VERSION_URL = "https://api.atlasacademy.io/info";
 export const ATLAS_JP_SVT_URL =
   "https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json";
@@ -9,8 +7,17 @@ export const ATLAS_NA_SVT_URL =
 export const MIN_SERVANT_LEVEL = 1 as const;
 export const MAX_SERVANT_LEVEL = 120 as const;
 
-export const MIN_ASCENSION_LEVEL: AscensionLevel = 0 as const;
-export const MAX_ASCENSION_LEVEL: AscensionLevel = 4 as const;
+export const ASCENSION_LEVELS = [0, 1, 2, 3, 4] as const;
+
+export const MIN_ASCENSION_LEVEL = ASCENSION_LEVELS.reduce(
+  (previousValue, currentValue) =>
+    previousValue < currentValue ? previousValue : currentValue,
+);
+
+export const MAX_ASCENSION_LEVEL = ASCENSION_LEVELS.reduce(
+  (previousValue, currentValue) =>
+    previousValue < currentValue ? previousValue : currentValue,
+);
 
 export const MIN_SKILL_LEVEL = 1 as const;
 export const MAX_SKILL_LEVEL = 10 as const;
@@ -21,5 +28,3 @@ export const MAX_APPEND_LEVEL = 10 as const;
 export const SKILL_NUMBERS = [1, 2, 3] as const;
 
 export const APPEND_SKILL_NUMBERS = [100, 101, 102] as const;
-
-export const ASCENSION_LEVELS = [0, 1, 2, 3, 4] as const;
